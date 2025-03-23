26893099b7b9fff9fc0471cf77fda0bfe802effe
carlos <carloskip25@gmail.com>
2025-03-23 16:43:28 +0300

    code grade

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// 1. Variable to multiply num1 and num2, and result should be 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // This will give 62
+
+// 2. Variable to generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; // Generates a random integer greater than 0
+
+// 3. Variable to calculate the remainder of num3 divided by num4, remainder should be 4
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; // This will give a remainder of 4
+
+// 4. Variable to find the maximum value in a set
+let values = [10, 15, 20, 5];
+let max = Math.max(...values); // This will return 20
